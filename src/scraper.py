e11f50b86a1a8e1ac66eb3157a0baa576d65bf10
issuebombom <issuebombom@issuebombomui-MacBookPro.local>
2024-07-22 11:55:03 +0900

    feat: add schedule screenshot

--- src/scraper.py
+++ src/scraper.py
@@ -1,17 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from src import logger
+from datetime import datetime as dt
 import time
 import requests
 import json
 
 
-def get_cookies(wings_target_url, company_id, user_id, password):
+def gw_login(wings_target_url, company_id, user_id, password):
 
     # Chrome WebDriver 경로 설정 (적절한 경로로 변경하세요)
     # chrome_driver_path = '/path/to/chromedriver'
@@ -21,6 +22,7 @@ def get_cookies(wings_target_url, company_id, user_id, password):
     chrome_options.add_argument("--headless")  # 브라우저를 표시하지 않음
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("window-size=1920,1080")
 
     # WebDriver 서비스 설정
 
@@ -47,6 +49,9 @@ def get_cookies(wings_target_url, company_id, user_id, password):
     # 로그인 처리를 기다림 (적절한 방법으로 대기 시간을 설정하거나 조건을 설정하세요)
     time.sleep(5)  # 예시로 5초 대기
 
+    return driver
+
+def get_cookies(driver: WebDriver, quit: bool=False):
     # 쿠키 가져오기
     cookies = driver.get_cookies()
     cookies_dict = {}
@@ -55,7 +60,8 @@ def get_cookies(wings_target_url, company_id, user_id, password):
         # print(f"Name: {cookie['name']}, Value: {cookie['value']}")
 
     # WebDriver 종료
-    driver.quit()
+    if quit:
+        driver.quit()
 
     return cookies_dict
 
@@ -114,3 +120,52 @@ def get_reference_preview(fnc_name_org, fnc_rsvn_no, event_no, session_id):
         return [row["TEXT"].strip() + "\n" for row in preview["rows"]]
     else:
         return [""]
+
+def get_schedule_screenshot(driver: WebDriver, file_path: str, quit: bool=True):
+    """로그인 후 스케줄 스크린샷
+    """
+    # 별표 클릭
+    driver.find_element(By.ID, "btnCat").click()
+    time.sleep(0.5)
+
+    # 연회 달력 클릭
+    driver.find_element(By.CSS_SELECTOR, "#treeView1 > li > ul > li:nth-child(2) > span.m_txt").click()
+    time.sleep(0.5)
+
+    # iframe 전환
+    iframe = driver.find_element(By.CSS_SELECTOR, 'iframe')
+    driver.switch_to.frame(iframe)
+
+    # ALL 클릭
+    driver.find_element(By.ID, "frmS_All").click()
+
+    # CXL 클릭(제외)
+    driver.find_element(By.ID, "frmS_Cxl").click()
+
+    # 오늘 날짜 시간 출력
+    today = str(dt.now()).split()[0] # ex. 2024-07-12
+
+    # 조회 날짜 선택
+    script = f'document.getElementById("frmS_datePic").value = "{today[:7]}";'
+    driver.execute_script(script)
+
+    # 조회 버튼 클릭
+    driver.find_element(By.ID, "comm_btn_find").click()
+    time.sleep(1)
+
+    # 크롬창 최대화
+    driver.maximize_window()
+    time.sleep(1)
+
+    # 최하단으로 스크롤 (최하단에 위치한 태그를 지정해서 해당 태그가 보이도록 스크롤하는 방식
+    low_element = driver.find_elements(By.CLASS_NAME, 'mv_row_container')[-1]
+    # 해당 요소로 스크롤하기
+    driver.execute_script("arguments[0].scrollIntoView(true);", low_element)
+
+    # 스크린샷 캡처
+    driver.save_screenshot(file_path)
+    time.sleep(0.5)
+
+    if quit:
+        # 브라우저 닫기
+        driver.quit()
