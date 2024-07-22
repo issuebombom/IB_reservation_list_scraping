33a2a5ae51bec7edb9f4be7ee271f91eb01a81ff
issuebombom <issuebombom@issuebombomui-MacBookPro.local>
2024-07-22 11:58:10 +0900

    fix: apply changed login func & screenshot func

--- daily_schedule_report.py
+++ daily_schedule_report.py
@@ -1,4 +1,5 @@
 from src import notion_api
+from src import scraper
 from src.slack_api import SlackAPI
 from src.logger import get_logger
 from config import ENV
@@ -6,9 +7,13 @@ from datetime import datetime as dt
 from datetime import timedelta
 
 
-def run(start_date, end_date):
+def run(start_date, end_date, driver):
     logger = get_logger("daily_schedule_report")
     logger.info(f"=====START DAILY REPORT SCRAPE=====")
+
+    # 스케줄 스크린샷 생성 (보류)
+    # screenshot_file_path = f'./screenshot/schedule_screenshot_{start_date[:7]}.png'
+    # scraper.get_schedule_screenshot(driver, screenshot_file_path, quit=True)
 
     notion_properties = notion_api.get_notion_properties_by_date(start_date, end_date, NOTION_API_HEADERS, NOTION_DATABASE_ID)
     subject, prompt = slack_daliy_schedule_report_bot.search_event_result_alarm_prompt(notion_properties)
@@ -25,9 +30,12 @@ if __name__ == "__main__":
     # 봇 생성
     slack_daliy_schedule_report_bot = SlackAPI(ENV["SLACK_TODAY_CHANNEL_ID"], ENV["SLACK_BOT_TOKEN"])
 
+    # driver 획득
+    driver = scraper.gw_login(ENV["GW_TARGET_URL"], ENV['GW_COMPANY_ID'], 
+                              ENV['GW_USER_ID'], ENV['GW_USER_PW'])
     # 오늘 날짜를 기준으로 오늘 ~ 내일을 스크랩 범위로 설정
     today = dt.now()
     start_date = today.strftime("%Y-%m-%d")
     end_date = (today + timedelta(days=1)).strftime("%Y-%m-%d")
 
-    run(start_date, end_date)
+    run(start_date, end_date, driver)
